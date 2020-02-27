43191e9cd9bc8526058ba34527a409ee124937ae
iscii <iscii@users.noreply.github.com>
2020-02-27 18:44:50 -0500

    lesson updates + github pages support

---
+++ Level 6/Lesson 6.3/itemdata.js
@@ -0,0 +1,10 @@
+pageTitle = "Dragons";
+
+itemList = [
+            {itemName: "Rax", itemInfo: "Rax was a dragon scout who was betrayed by one of his own. Killed before his time, Raax returned as a skeleton to continue the fight.", itemImage: "rax copy.png"},
+            {itemName: "Ash", itemInfo: "Born in flame, Ash uses the heat of his body to wound his enemies.", itemImage: "ash copy.png"},
+            {itemName: "Barnabas", itemInfo: "Bananas is the oldest and most respected of the dragons. He is wise beyong mortal years.", itemImage: "barnabas copy.png"},
+            {itemName: "Drake", itemInfo: "Master of the flame, Drake can fire a molten jet at distant enemies.", itemImage: "drake copy.png"},
+            {itemName: "Endisun", itemInfo: "With electricity coursing through his body at all times, Endisun is not only a deadly foe, but he can shoot into and out of the field of battle without his opponents even noticing.", itemImage: "endisun copy.png"},
+            {itemName: "Lunar", itemInfo: "Lunar's magical healing abilities make her a formidable opponent.", itemImage: "lunar copy.png"}
+        ];
