a2039e93f223d95cc6bbd62a774bc4b84ad159f4
iscii <iscii@users.noreply.github.com>
2020-07-03 12:14:10 -0400

    Lessons 4 and 5

---
+++ Level 8/Lesson 8.5/webserver.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var content = require("./content");
+
+http.createServer(function(request, response){
+    var pathname = url.parse(request.url).pathname.substring(1);
+    console.log("Request for " + pathname + " received.");
+
+    var ext = pathname.substring(pathname.indexOf(".") + 1); //takes pathname, returns the string after the "." character (filetype)
+    var contentType = content.getFileTypeObject(ext);
+
+    fs.readFile(pathname, function(err, data){
+        if(err){
+            console.log(err);
+            response.writeHead(404, {"Content-Type": contentType.type + "/" + contentType.extension});
+        }
+        else{
+            response.writeHead(404, {"Content-Type": contentType.type + "/" + contentType.extension});
+            
+            if(contentType.type != "text"){
+                response.write(data, "binary");
+            }
+            else{
+                response.write(data.toString());
+            }
+        }
+        response.end();
+    });
+}).listen(8081);
+
+console.log("Server running at http://127.0.01.1:8081")
