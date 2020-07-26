442eef2c17b0cd0c1428b92e514c65e198084c0c
iscii <iscii@users.noreply.github.com>
2020-07-26 12:35:14 -0400

    7/26/2020 - pet selection
    - data.json
        - getdata path
        - select currentpet from menu

--- Level 8/project/clientdata.js
+++ Level 8/project/clientdata.js
@@ -5,7 +5,26 @@ const url = require("url");
 exports.getClientData = function(pathname, request){
     console.log("pathname = " + pathname);
     switch(pathname){
-        case "pets":
+        case "getdata":
+            var data = fs.readFileSync("./js/data.json").toString();
+            return data;
+
+        case "writedata":
+            var qdata = parseQueryString(request);
+            if(qdata.file = "data"){
+                var data = JSON.parse(fs.readFileSync("./js/data.json").toString());
+                var prevdata = data[qdata.property];
+                data[qdata.property] = qdata.value;
+                console.log(data);
+                fs.writeFileSync("./js/data.json", JSON.stringify(data, null, 4), function(err){
+                    if(err)
+                        throw err;
+                    console.log("Updated " + qdata.file + "'s from [" + prevdata + "] to [" + qdata.value + "]");
+                });
+            }
+        break;
+
+        case "getpets":
             var pets = fs.readdirSync("./js/Pets");
             var petdata = [];
             for(item in pets){
@@ -13,60 +32,25 @@ exports.getClientData = function(pathname, request){
             }
             console.log(petdata);
             return JSON.stringify(petdata); //turns the array into a string. Apparently only strings can be returned in AJAX? I always get a JSON error
-        /*
-        case "petdata":
-            var qdata = parseQueryString(request);
-            var data = fs.readFileSync("./js/Pets/" + qdata.type + "_" + qdata.name + ".json");
-            return data.toString(); */
     
         case "create":
             var qdata = parseQueryString(request);
-            var frames = JSON.parse(fs.readFileSync("frame.json").toString());
+            var frames = JSON.parse(fs.readFileSync("./js/frame.json").toString());
             
             for(item in frames){
                 if(frames[item].type == qdata.type){
                     frames[item].name = qdata.name;
-                    var stats = JSON.stringify(frames[item]);
+                    var stats = JSON.stringify(frames[item], null, 4);
                 }
             }
 
-            fs.appendFile("js/Pets/" + qdata.type + "_" + qdata.name + ".json", stats, function(err){ //apppendfile, writefile?
+            fs.appendFile("./js/Pets/" + qdata.type + "_" + qdata.name + ".json", stats, function(err){ //apppendfile, writefile?
                 if(err)
                     throw err;
                 console.log("Created " + qdata.name + " of " + qdata.type);
             });
             return stats;
     }
-    /*
-    if(pathname == "petframe"){
-        var data = fs.readFileSync("frame.json"); //reads data as binary buffer
-        return data.toString(); //convert buffer to string to be split in getData() function
-    }
-    if(pathname == "pet"){
-        console.log("got it");
-    
-        var qdata = parseQueryString(request);
-        var frames = JSON.parse(fs.readFileSync("frame.json").toString());
-        
-        for(item in frames){
-            if(frames[item].type == qdata.type){
-                frames[item].name = qdata.name;
-                var stats = JSON.stringify(frames[item]);
-            }
-        }
-
-        fs.appendFile("js/Pets/" + qdata.type + "_" + qdata.name + ".json", stats, function(err){ //apppendfile, writefile?
-            if(err)
-                throw err;
-            console.log("Created " + qdata.name + " of " + qdata.type);
-        });
-        return stats;
-    }
-    if(pathname == "filecount"){
-
-    }
-    */
-
     return "";
 }
 function parseQueryString(request){
--- Level 8/project/js/script.js
+++ Level 8/project/js/script.js
@@ -12,22 +12,29 @@ function init(){
     selected = null;
     initialize = true;
     
-    ajax("pets"); //this ajax call must be first in order to create the pets variable for display()
+    ajax("getpets"); //this ajax call must be first in order to create the pets variable for display()
+    ajax("getdata");
 }
 function createSelect(type){
     selected = type.toUpperCase();
-    display();
+    displayMenu();
 }
 
-function ajax(tag){
+function selectPet(filename){
+    console.log("select");
+    ajax("writedata", "data", "currentPet", filename);
+    togglePop("menu");
+}
+
+function ajax(tag, file, property, value){
     console.log("getting data from " + tag);
     var request = new XMLHttpRequest();
     var url = "http://localhost:8081/";
     if(tag == "create"){
         url += "create?type=" + selected.toLowerCase() + "&name=" + nameForm.name.value;
     }
-    else if(tag == "petdata"){
-        url += "pet?type=" + type + "&name=" + name;
+    else if (tag == "writedata"){
+        url += "writedata?file=" + file + "&property=" + property + "&value=" + value; //file = pet json or data json
     }
     else{
         url += tag;
@@ -40,27 +47,41 @@ function ajax(tag){
         if(request.readyState == 4){
             var data = request.responseText;
 
-            if(tag == "pets"){
+            if(tag == "getdata"){
+                gamedata = JSON.parse(data);
+                for(item in pets){
+                    if(pets[item].type + "_" + pets[item].name == gamedata.currentPet){
+                        currentPet = pets[item];
+                    }
+                }
+                console.log(gamedata);
+                displayPet();
+            }
+            if(tag == "writedata"){
+                if(file == "data") ajax("getdata");
+                else ajax("getpets");
+            }
+            if(tag == "getpets"){
                 pets = JSON.parse(data);
                 for(item in pets){
                     pets[item] = JSON.parse(pets[item]);
                 }
-                if(initialize) currentPet = pets[0];
-                initialize = false;
                 console.log(pets);
+                displayMenu();
             }
             if(tag == "create"){
-                currentPet = JSON.parse(data);
+                if(!currentPet) ajax("writedata", "data", "currentPet", JSON.parse(data).type + "_" + JSON.parse(data).name);
                 togglePop("create");
-                ajax("pets");
+                ajax("getpets");
+                displayMenu();
             }
-            display();
         }
     }
     request.send();
 }
 
 function togglePop(type){
+    console.log("pop! " + type);
     if(type == "menu") var target = opMenuSet;
     if(type == "create") var target = opCreate;
 
@@ -70,15 +91,18 @@ function togglePop(type){
     
     if(type == "menu" && opCreate.style.display == "flex") opCreate.style.display = "none";
 }
-function display(){
+function displayMenu(){
     //petsmenu
     if(pets[0]){
-        opPet.src = currentPet.basic.image;
         opMenu.innerHTML = "";
         for(item in pets){
             var image = document.createElement("img");
             image.className = "menuimages";
+            image.id = pets[item].type + "_" + pets[item].name;
             image.src = pets[item].basic.image;
+            image.onclick = function(){
+                selectPet(this.id);
+            }
             opMenu.appendChild(image);
         }
     }
@@ -88,4 +112,7 @@ function display(){
         document.getElementById(NEWPETS[item]).style.border = "2px solid black";
         if(item == eval(selected)) document.getElementById(NEWPETS[item]).style.border = "2px solid white";
     }
+}
+function displayPet(){
+    opPet.src = currentPet.basic.image;
 }
